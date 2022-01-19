6f396e8458da6b0c4f1b058a749d09662842b155
Jay5339 <jrp5252@gmail.com>
2022-01-18 20:55:47 -0400

    Dice game project

---
+++ index.js
@@ -0,0 +1,21 @@
+var randomNumber1 = Math.random() *6;
+randomNumber1 = Math.floor(randomNumber1+1);
+
+var randomNumber2 = Math.random() *6;
+randomNumber2 = Math.floor(randomNumber2+1);
+
+document.querySelector("img.img1").attributes;
+document.querySelector("img.img1").setAttribute("src","images/dice" + randomNumber1 +".png");
+
+document.querySelector("img.img2").attributes;
+document.querySelector("img.img2").setAttribute("src","images/dice" + randomNumber2 +".png");
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").textContent = "🚩Player1 Wins!";
+}
+else if (randomNumber2>randomNumber1){
+    document.querySelector("h1").textContent = "Player2 Wins🚩!";
+}
+else{
+    document.querySelector("h1").textContent = "Draw!";
+}
